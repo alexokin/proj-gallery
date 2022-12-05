febd4b3683d1b65e456e2a6bf27b93c9b6fc872d
alexokin <alex.okin@gmail.com>
2022-12-05 17:01:43 +0200

    added rendering for modal

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
+'use strict'
 console.log('Starting up');
+
+
+$(onInit)
+
+function onInit() {
+    renderProjs()
+    console.log('page is ready');
+}
+
+function renderProjs() {
+    var projectMap = getProjs()
+    console.log('projects', projectMap)
+    var strHTMLs = projectMap.map(project => `
+    <div class="portfolio-modal modal fade" id="${project.id}" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="close-modal" data-dismiss="modal">
+          <div class="lr">
+            <div class="rl"></div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <div class="modal-body">
+                <!-- Project Details Go Here -->
+                <h2>${project.name}</h2>
+                <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                <img class="img-fluid d-block mx-auto" src="img/portfolio/${project.id}.jpg" alt="">
+                <p>${project.desc}</p>
+                <ul class="list-inline">
+                  <li>Date: January 2017</li>
+                  <li>Client: Explore</li>
+                  <li>Category: ${project.labels}</li>
+                </ul>
+                <button class="btn btn-primary" data-dismiss="modal" type="button">
+                    <i class="fa fa-times"></i>
+                    Close Project</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`)
+  $('.projects-container').html(strHTMLs)
+//   addCarEventListeners()
+}
+
---
+++ js/proj-service.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var gProjs = [
+  {
+    id: "minesweeper",
+    name: "Minesweeper",
+    title: "Better push those boxes",
+    desc: "lorem ipsum lorem ipsum lorem ipsum",
+    url: "https://alexokin.github.io/minesweeper-sprint1/",
+    publishedAt: 1448693940000,
+    labels: ["Matrixes", "keyboard events"],
+  }
+];
+
+
+function getProjs() {
+    var projects = gProjs
+    return projects
+}
